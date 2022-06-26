b0ff7007df3d0f2c5b0e04cff2ea3126eb986701
M-java86 <mpythondata@outlook.com>
2022-06-26 16:50:28 -0400

    added contructor, getters and setters

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,27 @@
 
 public class Car {
-
+	
+	//private fields
+	private String _model;
+	private String _make;
+	private int _year;
+	
+	//Constructor 
+	public Car(String make, String model, int year)
+	{
+		
+	}
+      //getters -simply return model
+	public String getModel() { return _model; }
+	public String getMake() { return _make; }
+	public int getYear() { return _year; } 
+	
+	//setters- take the string of a model as an argument and it will set it to the model.
+	//I have set a value to my model and passing the value as an argument.
+	//I use the _ under my private class variables so I can distinguish which one is my private variable and which one is an 
+	//argument that I'm passing 
+	
+	public void setModel(String model) { _model = model; }
+	public void setMake(String make) { _make = make; }
+	public void setYear(int year) { _year = year; }
 }
