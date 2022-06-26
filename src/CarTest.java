913f5c4b6dd0ecdc7680b6752d38aaa18d01fffd
M-java86 <mpythondata@outlook.com>
2022-06-26 18:00:20 -0400

    adding note

--- src/CarTest.java
+++ src/CarTest.java
@@ -5,6 +5,7 @@ public class CarTest {
 	public static void main(String[] args) {
 	//using a while loop and letting the user decide when to quit the program.
 		boolean quit = false;
+		//catch input variable
 		String model, make;
 		int year = 0;
 		Scanner scan = new Scanner(System.in);
